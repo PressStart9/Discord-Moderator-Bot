7b0b9389ad2e07ea8ac34e5c285bad8bb30b2721
PressStart9 <63587513+PressStart9@users.noreply.github.com>
2021-04-16 10:14:31 +0300

    Update parse.py

--- parse.py
+++ parse.py
@@ -627,7 +627,11 @@ async def check_time():
 
         for num in range(9):
             if (datetime.datetime.strptime(elements_game[num].find('time', 'entry-date published').attrs['datetime'], '%Y-%m-%dT%H:%M:%S+03:00') - datetime.datetime(1970, 1, 1)).total_seconds() > last_game and elements_game[num].find("span", "entry-cats").find_all("a")[1].text == 'Активная':
-                embed = discord.Embed(title=elements_game[num].find("h2", "entry-title").a.text, url=elements_game[num].find("div", "entry-content").p.text.split('Страница раздачи: ')[1].split()[0])
+                url = elements_game[num].find("div", "entry-content").p.text.split('Страница раздачи: ')[1].split()[0]
+                if url == url.replace('http', ''):
+                    embed = discord.Embed(title=elements_game[num].find("h2", "entry-title").a.text)
+                else:
+                    embed = discord.Embed(title=elements_game[num].find("h2", "entry-title").a.text, url=url)
                 img = elements_game[num].find('img', 'attachment-banner-small-image size-banner-small-image wp-post-image lazyloaded')
                 if img is None:
                     img = elements_game[num].find('img', 'attachment-banner-small-image size-banner-small-image wp-post-image lazyload')
