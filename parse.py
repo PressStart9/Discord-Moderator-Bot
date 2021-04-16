c11bba7926554991bcc72993adb195e43feb9e2d
PressStart9 <63587513+PressStart9@users.noreply.github.com>
2021-04-16 08:53:12 +0300

    Update parse.py

--- parse.py
+++ parse.py
@@ -633,7 +633,9 @@ async def check_time():
                 for guild in cursor.fetchall():
                     if guild[0] != 0:
                         channel = client.get_channel(guild[0])
-                        await channel.send(embed=embed.set_image(url=elements_game[num].find("a", "thumb-link").img.attrs['src']))
+                        print(elements_game[num].find("a", "thumb-link"))
+                        print(elements_game[num].find("a", "thumb-link").find('img', 'attachment-banner-small-image size-banner-small-image wp-post-image lazyloaded'))
+                        await channel.send(embed=embed.set_image(url=elements_game[num].find("a", "thumb-link").find('img', 'attachment-banner-small-image size-banner-small-image wp-post-image lazyloaded').attrs['src']))
                 last_game = (datetime.datetime.strptime(elements_game[num].find('time', 'entry-date published').attrs['datetime'], '%Y-%m-%dT%H:%M:%S+03:00') - datetime.datetime(1970, 1, 1)).total_seconds()
                 await asyncio.sleep(3)
 
